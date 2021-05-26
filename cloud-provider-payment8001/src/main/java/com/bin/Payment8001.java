3093963e444e1ad2ab67a688ed223592ec40ec2d
DESKTOP-R71C5UD\fitz <zjtxchen@gmail.com>
2021-05-26 17:54:10 +0800

    cloud项目构建

---
+++ cloud-provider-payment8001/src/main/java/com/bin/Payment8001.java
@@ -0,0 +1,12 @@
+package com.bin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Payment8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8001.class,args);
+    }
+}
